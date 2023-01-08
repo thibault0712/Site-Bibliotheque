74b6e88d84d7c48cb53c8c438c0e476c7287a467
Thibault <76521322+thibault0712@users.noreply.github.com>
2023-01-08 21:15:20 +0100

    Update mysqlConnection.php

--- config/mysqlConnection.php
+++ config/mysqlConnection.php
@@ -1,9 +1,9 @@
 <?php
 
-    $host = '192.168.0.179:3306'; // Hôte de la base de données
-    $user = 'Users'; // Nom d'utilisateur de la base de données
-    $password = 'Stjoyrhup@29'; // Mot de passe de la base de données
-    $database = 'phpmyadmin'; // Nom de la base de données
+    $host = 'ipAdresse'; // Hôte de la base de données
+    $user = 'User'; // Nom d'utilisateur de la base de données
+    $password = 'password'; // Mot de passe de la base de données
+    $database = 'name_of_the_database'; // Nom de la base de données
 
     // Création de la connexion
     $conn = mysqli_connect($host, $user, $password, $database);
