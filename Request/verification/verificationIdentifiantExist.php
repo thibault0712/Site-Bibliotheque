1cdb46dccd41fadd8b757dbf58f50001c22830f5
Thibault <76521322+thibault0712@users.noreply.github.com>
2023-01-20 18:29:13 +0100

    Update verificationIdentifiantExist.php

--- Request/verification/verificationIdentifiantExist.php
+++ Request/verification/verificationIdentifiantExist.php
@@ -1,27 +1,27 @@
 <?php
     function verificationIdentifiantExist($identifiant){
         include "../config/mysqlConnection.php";
-        // Préparez une requête SQL de sélection pour vérifier si un utilisateur avec un nom d'utilisateur spécifique existe dans la table "users"
+        // Préparez une requête SQL de sélection pour vérifier si un utilisateur avec un nom d'utilisateur spécifique existe dans la table "Bibliotheque"
         $sql = "SELECT * FROM Bibliotheque WHERE Identifiant=?";
         $stmt = mysqli_prepare($conn, $sql);
 
-        // Liens une variable à la requête préparée en tant que paramètre
+        // On rentre le paramètre identifiant dans la requête sql
         mysqli_stmt_bind_param($stmt, "s", $identifiant);
 
         // Exécutez la requête préparée
         mysqli_stmt_execute($stmt);
 
-        // Obtenez le résultat de la requête
+        // Obtention du résultat
         $result = mysqli_stmt_get_result($stmt);
 
-        // Vérifiez combien de lignes ont été retournées
+        // On conte le nombre de ligne retrouvé
         if (mysqli_num_rows($result) > 0) {
             return true;
        } else {
             return false;
         }
 
-        // N'oubliez pas de fermer le déclencheur lorsque vous avez terminé
+        // On ferme la connection
         mysqli_stmt_close($stmt);
     }
 ?>
