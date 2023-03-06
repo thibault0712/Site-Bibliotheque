bdba87e6efa73a8123e161abdea82b6c731140e1
Thibault <76521322+thibault0712@users.noreply.github.com>
2023-03-06 19:15:19 +0100

    Update request.php

--- Request/request.php
+++ Request/request.php
@@ -3,10 +3,11 @@
     include "verification/verificationIdDefine.php";
     include "verification/verificationDatabaseDisponible.php";
     include "verification/verificationIdentifiantExist.php";
+    include "getElements/size.php";
 
     if (verificationIdDefine() == true && verificationDatabaseDisponible($conn) == true){
         if (verificationIdentifiantExist($_GET['identifiant']) == true){
-            echo "true";
+            echo getSize();
         }else{
             echo "false";
         }
