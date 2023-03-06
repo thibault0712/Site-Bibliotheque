55e0032488c9d77964be860e84fcd2ec335cc873
Thibault <76521322+thibault0712@users.noreply.github.com>
2023-03-06 19:13:39 +0100

    Update size.php

--- Request/getElements/size.php
+++ Request/getElements/size.php
@@ -1,1 +1,13 @@
-
+<?php
+    function getSize($identifiant){
+        include "../config/mysqlConnection.php";
+        $sql = 'SELECT * FROM `Bibliotheque` WHERE `Identifiant` == $identifiant';
+        $result = mysql_query($sql, $conn) or die(mysql_error());
+        $total = mysql_num_rows($result);
+        if($total == 1) {
+          return $row['Size'];
+        }else{
+          return false;
+        }
+    }
+?>
